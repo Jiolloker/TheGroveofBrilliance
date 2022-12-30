dd5a8313dbd8e0d787c9d2ba2b4702577285e266
Jiolloker <jiolloker@hotmail.com>
2022-12-30 16:59:49 -0300

    my first commit

---
+++ git.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#Usage of script... 
+#Syntax
+#bash git.sh username access_token
+
+
+USER=$1
+GITHUB_TOKEN=$2
+echo "Process of clonning repositories."
+echo "This script clones all repositories, this includes the private repositories."
+sleep 3
+readarray array <<< $(curl -X GET -u $GITHUB_TOKEN:x-oauth-basic https://api.github.com/users/$USER/repos | grep -e 'clone_url*' | cut -d \" -f 4)
+
+mkdir -p ~/git && cd ~/git
+
+for element in ${array[@]}
+do
+  echo "clonning $element"
+  git clone $element
+done
+
